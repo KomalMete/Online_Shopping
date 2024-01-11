ac171ae3f5d1c25d3c6f4fe7ec4dd8bc2b03fc83
KomalMete <komalmete8@gmail.com>
2024-01-11 16:10:02 +0530

    customer controller started

---
+++ backend/src/main/java/com/app/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.app.service;
+
+import java.util.List;
+
+import com.app.entity.Customer;
+
+
+public interface CustomerService {
+
+	List<Customer> allCustomers();
+
+	Customer saveCustomer(Customer customer);
+
+	Customer getCustomerById(int id);
+
+	String removeCustomer(int id);
+
+	
+
+}
