069d4f71c28460022c2fc19bc2b1073448502b16
KomalMete <komalmete8@gmail.com>
2024-01-19 23:25:07 +0530

    commit

--- backend/src/main/java/com/app/controller/CartController.java
+++ backend/src/main/java/com/app/controller/CartController.java
@@ -1,6 +1,11 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,17 @@ public class CartController {
 
 	@Autowired
 	private CartService cartService;
+	
+	@GetMapping("getcartproducts/{customerId}")
+	public ResponseEntity<?> getCartProducts(@PathVariable int customerId)
+	{
+		return new ResponseEntity<>(cartService.getCartProducts(customerId), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteproductfromcart/{id}")
+	public ResponseEntity<?> removeFromCart(@PathVariable int id)
+	{
+		return new ResponseEntity<>(cartService.removeCartProduct(id), HttpStatus.OK);
+		
+	}
 }
