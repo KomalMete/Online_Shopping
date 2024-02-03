cd0c0b3bf03615305671e1e21e7659235de28b63
KomalMete <komalmete8@gmail.com>
2024-02-03 19:27:21 +0530

    update

--- frontend/src/Component/Login.js
+++ frontend/src/Component/Login.js
@@ -2,30 +2,53 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { url } from "./Common/constants";
+import "./Login.css"
 
 const Login =() => {
+
+    const [email, setEmail] = useState();
+    const [password , setpassword] = useState();
+
+    const navigate = useNavigate();
+
+    const handleLogin=() =>{
+
+        const customer = {
+            email,
+            password
+        };
+
+       
+
+    }
+
     return(
         <div className=" position-absolute top-50 start-50 translate-middle loginback">
+            <h1>Welcome To Shopify</h1>
+            <img src="./Images/Shopify-Logo.png"></img>
             <div className="position-absolute top-50 start-50 translate-middle loginform">
-
-                <div className="p-5 text-whiteS">
+                
+                <div className="p-5 text-white">
                     <h1>
                         LOGIN
                     </h1>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email </label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={email} onChange={(e) => { setemail(e.target.value) }} />
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={email} onChange={(e) => { setEmail(e.target.value) }} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
                         <input type="password" className="form-control" id="password" value={password} onChange={(e) => { setpassword(e.target.value) }}/>
                     </div>
-                    <button type="submit" className="btn btn-primary" onClick={HandleLogin}>Submit</button>
+                    <button type="submit" className="btn btn-dark" onClick={handleLogin}>Submit</button>
                     <div>
-                        <Link className="btn btn-secondary m-3" to={"/register"}>REGISTER</Link>
+                        <Link className="btn btn-success m-3" to={"/register"}>REGISTER</Link>
                         <Link className="m-3" to={"/forgotpassword"}>Forgot Password</Link>
                     </div>
                 </div>
+
+                
             </div>
         </div>
     )
